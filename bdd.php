ce10c63fdb3b26a182bd9d98840bb5c0f47d4ad3
ReuS-SIO <265682254+ReuS-SIO@users.noreply.github.com>
2026-09-18 09:35:44 +1100

    Force le dev a utiliser son mdp

--- bdd.php
+++ bdd.php
@@ -20,7 +20,7 @@ $DB_HOST = getenv('DB_HOST') ?: detectDbHost();
 $DB_PORT = getenv('DB_PORT') ?: '5432';
 $DB_NAME = getenv('DB_NAME') ?: 'gestion_tournois_mc';
 $DB_USER = getenv('DB_USER') ?: 'postgres';
-$DB_PASS = getenv('DB_PASS') ?: 'postgres';
+$DB_PASS = getenv('DB_PASS') ?: 'ME_CHANGER'; # MET TON MDP À TOI :)
 
 try {
     $pdo = new PDO(
